e5eab6e6d30ec974970271f4aeacf2a91e3ccbfb
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-10-24 11:42:51 -0300

    feat: create get all user

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,5 +3,13 @@ import { Router } from "express";
 const usuariosRoutes = Router();
 
 let usuarios = [];
+usuariosRoutes.get("/", (req, res) =>{
+    return res.status(200).json({
+        message: usuarios.length == 0 
+        ? "Não há usuarios cadastrados"
+        : `total de usuarios: ${usuarios.length}`,
+        usuarios, 
+    });
+});
 
 export default usuariosRoutes;
